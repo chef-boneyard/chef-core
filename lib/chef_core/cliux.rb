81feeb4e84bb7072b9ec2569738f85f7f923b718
Marc A. Paradise <marc.paradise@gmail.com>
2019-02-15 10:55:16 -0500

    Use add_gem_localization to load i18n files
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- lib/chef_core/cliux.rb
+++ lib/chef_core/cliux.rb
@@ -3,9 +3,7 @@ require "chef_core/text"
 
 module ChefCore
   module CLIUX
-    GEM_LOCALIZATION_PATH = File.absolute_path(File.join(File.dirname(__FILE__), "..", "..", "i18n"))
-    # Initialize our own translations on load
-    ChefCore::Text.add_localization(GEM_LOCALIZATION_PATH)
+    ChefCore::Text.add_localization("chef_core-cliux")
   end
 end
 
