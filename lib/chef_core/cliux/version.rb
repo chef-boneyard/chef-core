719c386de5e6639343bc17f982f6f8d573549983
Marc A. Paradise <marc.paradise@gmail.com>
2019-04-10 08:36:16 -0400

    Keep gem versions in sync
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- lib/chef_core/cliux/version.rb
+++ lib/chef_core/cliux/version.rb
@@ -14,9 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require "chef_core/version"
 
 module ChefCore
   module CLIUX
-    VERSION = "0.0.1".freeze
+    VERSION = ChefCore::Version::VERSION
   end
 end
